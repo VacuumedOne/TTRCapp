0aeebf446ae749abe7eb620789e3ef863f703b61
VacuumedOne <nocks_on@okumuratadashihitoshinoMacBook-ea.local>
2018-12-16 01:23:37 +0900

    sql added

---
+++ server/local_modules/user/UserRegister.js
@@ -0,0 +1,108 @@
+module.exports = function(body, res, connection) {
+  var Promise = require('promise')
+
+  /**
+   * POST内容
+   * user_name: ログイン用のアルファベット名
+   * password: 素のパスワード。ハッシュ化前。
+   * mail: メールアドレス。
+   * sex: 性別。"male" or "female"
+   * auth: 権限。
+   * birth_ymd: 誕生日。
+   * k_lastname: 漢字の姓。
+   * k_firstname: 漢字の名。
+   * h_lastname: ひらがなの姓。
+   * h_firstname: ひらがなの名。
+   */
+  
+  /* 入力のバリデーション */
+  var err = [];
+  if(!body.user_name) {
+    err.push('ユーザ名が入力されていません。');
+  }
+  if(!body.password) {
+    err.push('パスワードが入力されていません。');
+  }
+  if(!body.mail) {
+    err.push('メールアドレスが入力されていません。');
+  }
+  if(!body.sex) {
+    err.push('性別が入力されていません。');
+  }
+  if(!body.auth) {
+    err.push('役職が入力されていません。');
+  }
+  if(!body.k_lastname) {
+    err.push('姓が入力されていません。');
+  }
+  if(!body.k_firstname) {
+    err.push('名が入力されていません。');
+  }
+
+  if(err.length > 0) {
+    res.json(err);
+    return;
+  }
+
+  var sql = '';
+  sql += 'INSERT INTO User (' +
+         'user_name' +
+         ', hashed_pw' +
+         ', mail' +
+         ', sex' +
+         ', auth';
+  if(!body.birth_ymd) {
+    sql += ', birth_ymd';
+  }
+  sql += ', k_lastname' +
+         ', k_firstname';
+  if(!body.h_lasttname) {
+    sql += ', h_lastname';
+  }
+  if(!body.h_firstname) {
+    sql += ', h_firstname';
+  }
+  sql += ') VALUES (' +
+         body.user_name + 
+         ', ' + body.hashed_pw + 
+         ', ' + body.mail + 
+         ', ' + body.sex +
+         ', ' + body.auth;
+  if(!body.birth_ymd) {
+    sql += ', ' + body.birth_ymd;
+  }
+  sql += ', ' + body.k_lastname +
+         ', ' + body.k_firstname;
+  if(!body.h_lasttname) {
+    sql += ', ' + body.h_lasttname;
+  }
+  if(!body.h_firstname) {
+    sql += ', ' + body.h_firstname;
+  }
+  sql += ')';
+  
+  console.log('Query: ' + sql);
+
+  /* DB格納 */
+  var promise = new Promise(function(resolve, reject) {
+    connection.query('SHOW DATABASES;', (error, results, fields) => {
+      if(error){
+        reject(error);
+      }else{
+        resolve(results);
+      }
+    });
+  });
+
+  promise.then((results) => {
+    res.json(results);
+  }).catch((error) => {
+    throw error;
+  })
+
+  // connection.query('SHOW DATABASES;', function (error, results, fields) {
+  //   if(error) throw error;
+  //   console.log(results);
+  //   result = results;
+  // });
+}

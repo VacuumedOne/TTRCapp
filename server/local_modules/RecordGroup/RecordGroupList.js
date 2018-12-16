0184f8f2cc96cdf978d632dc4f52c1b391e78905
VacuumedOne <nocks_on@okumuratadashihitoshinoMacBook-ea.local>
2018-12-16 17:21:30 +0900

    RecordGroupRegister を追加

--- server/local_modules/RecordGroup/RecordGroupList.js
+++ server/local_modules/RecordGroup/RecordGroupList.js
@@ -1,3 +1,36 @@
 module.exports = function(body, res, connection) {
-  
+  var Promise = require('promise');
+
+  /**
+   * POST内容
+   * group_name: 記録グループ名。
+   */
+
+   //入力のバリデーション
+   console.log(body);
+   if(!body.group_name) {
+     res.json(['グループ名が入力されていません。']);
+     return;
+   }
+
+   //SQL発行
+   var sql = 'INSERT INTO RecordGroup (group_name) VALUES ("' + body.group_name + '";';
+
+   //DB格納
+   var promise = new Promise(function(resolve, reject) {
+    connection.query(sql, (error, results, fields) => {
+      if(error){
+        reject(error);
+      }else{
+        resolve(results);
+      }
+    });
+  });
+
+  promise.then((results) => {
+    res.json(results);
+  }).catch((error) => {
+    throw error;
+  });
+  return;
 }

9a8e3ef6bdd0290990bbcce9115fd33460221f02
VacuumedOne <nocks_on@okumuratadashihitoshinoMacBook-ea.local>
2019-01-14 16:04:06 +0900

    ライブラリ追加

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import { MdButton, MdContent, MdTabs, MdCard } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdCard)
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

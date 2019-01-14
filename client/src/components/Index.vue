3ab478937ed34630fa537bcd17d8d600e28ac8b8
VacuumedOne <nocks_on@okumuratadashihitoshinoMacBook-ea.local>
2019-01-14 17:27:57 +0900

    サイドバーを作成

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="index">
     <!-- サイドメニュー -->
-
-    <!-- ヘッダー(メニュー起動ボタンとロゴ等) -->
+    <side-bar class="sidebar"></side-bar>
 
     <!-- メインタブ -->
-    <main-tab class="main-tab"></main-tab>
+    <div class="main">
+      <main-tab class="main-tab"></main-tab>
+    </div>
   </div>
 </template>
 
 <script>
 import MainTab from './Index/MainTab'
+import SideBar from './Index/SideBar'
 
 export default {
   name: 'Index',
   components: {
-    'main-tab': MainTab
+    'main-tab': MainTab,
+    'side-bar': SideBar
   }
 }
 </script>
 
 <style lang="sass" scoped>
 .index
-  text-align: center
+  text-align: left
   height: 100%
   width: 100%
+  margin: 0px
+  display: table
+.sidebar
+  display: table-cell
+  width: 300px
+  height: 100%
+  vertical-align: top
+.main
+  display: table-cell
+  height: 100%
+  text-align: center
+  vertical-align: middle
 </style>
--- client/src/components/Index/SideBar.vue
+++ client/src/components/Index/SideBar.vue
@@ -1,18 +1,42 @@
 <template>
   <md-card class="sidebar">
-    
+    <md-card-header>
+      <div>MENU</div>
+    </md-card-header>
+    <md-card-content>
+      <md-card md-with-hover class="button">
+        年間予定
+      </md-card>
+      <md-card md-with-hover class="button">
+        ログイン管理
+      </md-card>
+      <md-card md-with-hover class="button">
+        メニュー管理
+      </md-card>
+      <md-card md-with-hover class="button">
+        Wiki
+      </md-card>
+    </md-card-content>
   </md-card>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
 <style lang="sass" scoped>
-.sidebar 
-  background-color: gray
-  color: white
-</style>
+$main: #333
+$accent: #EEE
 
+.sidebar
+  background-color: $main
+  color: $accent
+.button
+  width: 86%
+  padding: 7%
+  margin: 7%
+  background-color: $accent
+  color: $main
+</style>

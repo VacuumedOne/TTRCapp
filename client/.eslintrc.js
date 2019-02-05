a69c77416976f15f568a920b9f90e86c378bce38
VacuumedOne <nocks_on@okumuratadashihitoshinoMacBook-ea.local>
2019-02-05 18:30:52 +0900

    eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-console': 'error',
-    'block-spacing': 'off'
+    'block-spacing': 'off',
+    'spaced-comment': 'off'
   }
 }

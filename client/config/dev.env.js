a3e9ee2258257846f38295d9430ca9920cb17520
VacuumedOne <vacuum.naito.7010@gmail.com>
2019-02-20 21:06:46 +0900

    product用

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  VUE_APP_API_SERVER_BASE_URL: 'http://rowingkuramae.com',
+  VUE_APP_API_SERVER_BASE_URL: '"localhost:3000"',
   NODE_ENV: '"development"'
 })
--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
+  VUE_APP_API_SERVER_BASE_URL: '"http://rowingkuramae.com"',
   NODE_ENV: '"production"'
 }
